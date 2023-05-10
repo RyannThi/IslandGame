654fed4886101c48db782c8ef58bdb9d322bd3da
MrQvest <104365438+MrQvest@users.noreply.github.com>
2023-05-10 14:25:20 -0300

    Update diálogo
    adicionado um trigger para o cursor do mouse. O mouse precisa estar sobre um colisor com a tag NPC para funcionar

--- Assets/Scripts/Dialogue/DialogueTrigger.cs
+++ Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -5,6 +5,24 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Check if hit is NPC collider 
+                if (hit.collider.gameObject.CompareTag("NPC"))
+                {
+                    Debug.Log("NPC atingido");
+                    TriggerDialogue();
+                }
+            }
+        }
+    }
     
     public void TriggerDialogue()
     {
