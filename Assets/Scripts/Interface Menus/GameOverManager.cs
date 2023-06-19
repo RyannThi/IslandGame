77c787340480ac0e84a50ba05bf869a4d33c3f24
Ryann1908 <59339990+RyannThi@users.noreply.github.com>
2023-06-18 22:13:43 -0300

    Update GameOverManager.cs

--- Assets/Scripts/Interface Menus/GameOverManager.cs
+++ Assets/Scripts/Interface Menus/GameOverManager.cs
@@ -59,7 +59,7 @@ public class GameOverManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.None;
         fadeInCoroutine = StartCoroutine(FadeInGroup(mainGroup, mainGroupCanvasGroup, mainGroupInteract, (value) => mainGroupInteract = value));
         Time.timeScale = 0f;
     }
